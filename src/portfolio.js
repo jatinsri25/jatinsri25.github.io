5b13a8e417b79691ddd1cf50f52d48dec5fda533
Jatinsri25 <srivastavajatin9898@gmail.com>
2024-09-05 15:00:12 +0530

    Initial commit

---
+++ src/portfolio.js
@@ -0,0 +1,70 @@
+const header = {
+  homepage: 'https://jatinsri25.github.io/',
+  title: '</JS>',
+}
+
+const about = {
+  name: 'Jatin Srivastava',
+  role: 'Full Stack Developer',
+  description:
+    'Welcome to my portfolio! I am Jatin Srivastava, a Full Stack Developer specializing in the MERN stack where I craft dynamic and responsive web applications.I also have experience with GraphQL, PostgreSQL and Prisma ORM for efficient data handling and API development.Additionally, I delve into blockchain technology, creating decentralized applications(DApps) using Solidity.My main focus is delivering high - quality, full - featured web solutions using the MERN stack—fueled by way too much coffee, because who needs sleep when you have caffeine and code?',
+  resume: 'https://drive.google.com/file/d/1dOUrrJRjajqesPOk8eTbcre2Bq1i_LW9/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/jatinsri25/',
+    github: 'https://github.com/jatinsri25',
+  },
+}
+
+const projects = [
+  {
+    name: 'QuirkQuack',
+    description:
+      'Developed a real-time chat application using the MERN stack with TypeScript. Built an interactive React UI with instant messaging powered by Socket.IO. Streamlined database management using Prisma and PostgreSQL for improved performance and efficiency',
+    stack: ['React', 'TypeScript', 'Node.js', 'Socket.IO', 'Prisma', 'PostgreSQL'],
+    sourceCode: 'https://github.com/jatinsri25/QuirkQuack',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'AI Resume Builder',
+    description: 'Built an AI-driven resume builder using the MERN stack and Strapi, providing real-time content recommendations and formatting adjustments. Applied machine learning to generate tailored, professional resumes. Showcased skills in full-stack development, API integration, and user-centric design.',
+    stack: ['MERN', 'Strapi', 'Tailwind', 'Vite'],
+    sourceCode: 'https://github.com/jatinsri25/ResuMech',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Job Seeking Application',
+    description:
+      'Developed a full-stack job-seeking platform using the MERN stack, allowing users to create profiles, search for jobs, and apply seamlessly. Integrated real-time updates via Socket.IO, ensuring a smooth user experience. Enhanced platform efficiency with MongoDB, Express, React, and Node.js.',
+    stack: ['MongoDB', 'Express', 'Node.js', 'HTML', 'CSS'],
+    sourceCode: 'https://github.com/jatinsri25/Job-Seeking-Website',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'SASS',
+  'Git',
+  'Python',
+  'Java',
+  'C/C++',
+  'HTML/CSS',
+  'JavaScript',
+  'SQL',
+  'Node.js',
+  'Solidity',
+  'GraphQL',
+  'MongoDB',
+  'Prisma',
+  'Next.js'
+]
+
+const contact = {
+  email: 'srivastavajatin9898@gmail.com',
+}
+
+export { header, about, projects, skills, contact }

c76be5310ade3194e9fe93c433d069611898285c
JATIN SRIVASTAVA <93035514+jatinsri25@users.noreply.github.com>
2024-12-11 15:54:48 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rjshkhr/cleanfolio'
+      href='https://github.com/jatinsri25'
       className='link footer__link'
     >
       Created By Jatin Srivastava
